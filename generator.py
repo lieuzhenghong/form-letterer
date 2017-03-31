07972ee84e56fe3dab6e59d012bd2a28ecfe5d79
lieuzhenghong <lieuzhenghong@gmail.com>
2017-03-31 15:38:58 +0800

    Catch errors properly

--- generator.py
+++ generator.py
@@ -46,50 +46,65 @@ def replace_text(para, old, rep):
 
 
 def generate_letters(csv_filename, letter_filename):
-    # Read the file and put it into an array
     try:
-        with open('{}.csv'.format(csv_filename), newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            data = []
-            for idx, row in enumerate(reader):
-                if idx == 0:
-                    headers = row
-                else:
-                    data.append(row)
-    except FileNotFoundError as error:
-        print('<h2> ERROR: File {}.csv not found.'.format(csv_filename))
-        return('<h2> ERROR: File {}.csv not found.'.format(csv_filename))
-    except PermissionError as error:
-        return('<h2> PERMISSION ERROR: ' + str(error) '</h2><p>You have a file open and so the program cannot run. Make sure to close all csv files before running the program.') 
-    for idx, row in enumerate(data):
+        # Read the file and put it into an array
         try:
-            document = Document('{}.docx'.format(letter_filename))
-        except: 
-            print('ERROR: File {}.docx not found.'.format(letter_filename))
-            return('ERROR: File {}.docx not found.'.format(letter_filename))
-        for para in document.paragraphs:
-            m = re.findall(r'`(.+?)`', para.text)
-            for match in m:
-                # Find the start and end of the substring
-                try:
-                    rep = str(row[headers.index(match)])
-                except ValueError as error:
-                    return('<h2>VALUE ERROR: ' + str(error) + '</h2><p>This often means that your word document has a thing surrounded by backticks that does not exist in your CSV. Check again.')
-                n = '`'+match+'`'
-                replace_text(para, n, rep)
-
-        for para in document.paragraphs:
-            m = re.findall(r'{{(.+?)}}', para.text)
-            for match in m:
-                match2 = match.replace('‘', "'")
-                match2 = match2.replace('’', "'")
-                x = str(eval(match2))
-                replace_text(para, '{{' + match + '}}', x)
-        try:
-            document.save('./build/{0}_{1}.docx'.format(letter_filename, row[headers.index('name')]))
+            with open('{}.csv'.format(csv_filename), newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                data = []
+                for idx, row in enumerate(reader):
+                    if idx == 0:
+                        headers = row
+                    else:
+                        data.append(row)
         except PermissionError as error:
-            return('<h2> PERMISSION ERROR: ' + str(error) '</h2><p>You have a file open and so the program cannot run. Make sure to close all docx files before running the program.') 
-    return('OK')
+            return('<b> PERMISSION ERROR: ' + str(error) + '</b><p>You have a'
+                   'csv file open and so the program cannot run. Make sure to'
+                   'close all' 'csv files before running the program.')
+        except FileNotFoundError as error:
+            return('<b>FILE NOT FOUND ERROR</b>:'
+                   'File {}.csv not found.'.format(csv_filename))
+        for idx, row in enumerate(data):
+            try:
+                document = Document('{}.docx'.format(letter_filename))
+            except FileNotFoundError as error:
+                return('ERROR: File {}.docx not found.'.format(letter_filename))
+            except PermissionError as error:
+                return('<b>PERMISSION ERROR: ' + str(error) + '</b><p>'
+                       'You have a'
+                       'file open and so the program cannot run. Make sure to'
+                       'close\ all docx files before running the program.')
+            for para in document.paragraphs:
+                m = re.findall(r'`(.+?)`', para.text)
+                for match in m:
+                    # Find the start and end of the substring
+                    try:
+                        rep = str(row[headers.index(match)])
+                    except ValueError as error:
+                        return('<b>VALUE ERROR: ' + str(error) + '</b><p>This'
+                               'often\ means that your word document has a'
+                               'thing surrounded\ by backticks that does not'
+                               'exist in your CSV. Check\ again.')
+                    n = '`'+match+'`'
+                    replace_text(para, n, rep)
+
+            for para in document.paragraphs:
+                m = re.findall(r'{{(.+?)}}', para.text)
+                for match in m:
+                    match2 = match.replace('‘', "'")
+                    match2 = match2.replace('’', "'")
+                    x = str(eval(match2))
+                    replace_text(para, '{{' + match + '}}', x)
+            try:
+                document.save('./build/{0}_{1}.docx'.format(letter_filename,
+                              row[headers.index('name')]))
+            except PermissionError as error:
+                return('<b>PERMISSION ERROR: ' + str(error) + '</b><p>You have'
+                       'a\ file open and so the program cannot run. Make sure'
+                       'to close\ all docx files before running the program.')
+        return('OK')
+    except Exception as e:
+        return(str(e))
 
 if __name__ == '__main__':
     import sys
